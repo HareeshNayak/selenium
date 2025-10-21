83c4f6c0999bb371e24e38031ad2b9220f86a584
Hareesh Nayak <162031224+HareeshNayak@users.noreply.github.com>
2025-10-21 17:59:14 +0530

    changed

--- utils/browser.py
+++ utils/browser.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-def get_driver(browser_name="chrome"):
-    if browser_name == "chrome":
-        options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
-        driver = webdriver.Chrome(options=options)
-    elif browser_name == "edge":
-        driver = webdriver.Edge()
-    else:
-        raise Exception("Browser not supported")
+
+def get_driver():
+    options = Options()
+    options.add_argument("--headless")  # Run Chrome in headless mode
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
+    # Ensure Chrome does NOT try to reuse a local profile
+    options.add_argument("--user-data-dir=/tmp/unique-chrome-profile")
+
+    driver = webdriver.Chrome(options=options)
     return driver
